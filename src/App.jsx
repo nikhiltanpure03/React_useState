3815be7ec3a787a5f887aba778c7d22cbab00be4
Nikhil Tanpure <nikhiltanpure2003@gmail.com>
2026-03-10 10:46:27 +0530

    useState first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Counter from './Counter'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Hooks In React</h1>
+     <hr />
+
+     <Counter />
+    </>
+  )
+}
+
+export default App
