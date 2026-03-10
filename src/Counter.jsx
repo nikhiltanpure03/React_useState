f60b3b2adbea63acf6ae10ccd6af0f34811eec23
Nikhil Tanpure <nikhiltanpure2003@gmail.com>
2026-03-10 10:53:10 +0530

    space remove Counter.jsx by Nikhil

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,28 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
- 
-const Counter = ()=>{
-   
-    let [count,setCount] = useState(0);
-    function inCount(){
-       setCount(count+1)
-        console.log(count);
-        
-    }
-    let [countdec,setCountdec] = useState(0);
-    function deCount(){
-        setCountdec(countdec-1);
-        console.log(countdec);
-        
-        
-    }
+const Counter = () => {
+  let [count, setCount] = useState(0);
+  function inCount() {
+    setCount(count + 1);
+    console.log(count);
+  }
+  let [countdec, setCountdec] = useState(0);
+  function deCount() {
+    setCountdec(countdec - 1);
+    console.log(countdec);
+  }
 
-    return(
-        <div>
-            <h1>Counter Component</h1>
-            <button style={{backgroundColor:"greenyellow"}} onClick={inCount}>Count in :{count}</button><br /><br />
-            <button style={{backgroundColor:"red", fontFamily:"monospace"}} onClick={deCount}>Count dec : {countdec}</button> <br /> <br />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Counter Component</h1>
+      <button style={{ backgroundColor: "greenyellow" }} onClick={inCount}>
+        Count in :{count}
+      </button>
+      <br />
+      <br />
+      <button
+        style={{ backgroundColor: "red", fontFamily: "monospace" }}
+        onClick={deCount}
+      >
+        Count dec : {countdec}
+      </button>{" "}
+      <br /> <br />
+    </div>
+  );
+};
 export default Counter;
